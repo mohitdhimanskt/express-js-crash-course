012fecd62275f744c19d4a32514e32f1cca0b802
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-24 16:21:10 +0530

    Add to middlewars

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ const app = express();
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('veiw engine', 'handlbars');
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
