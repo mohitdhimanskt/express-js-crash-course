48447e98a1c14e8b87aaa3e72bcbc06a37cd8f45
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-25 17:34:20 +0530

    Add to create member

--- routes/api/members.js
+++ routes/api/members.js
@@ -16,6 +16,22 @@ router.get('/:id',(req, res)=>{
 
   }else{
     res.status(400).json({msg: `No member with the id of ${req.params.id}`});
-    
+
   }
-})
+});
+
+// Create Member
+router.post('/',(req, res)=>{
+  const newMember = {
+    ...req.body,
+    id: uuid.v4(),
+    status: 'active'
+  }
+
+if(!newMember.name || !newMember.email){
+  return res.status(400).json({msg: 'Please include a name and email' });
+}
+members.push(newMember);
+res.json(members);
+//res.redirect('/');
+});
